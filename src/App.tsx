a7ccbe8367aa9caa67fa440d34cd9ca01050bbdc
skillbridgebd <skillbridgebd@gmail.com>
2025-11-05 17:19:14 +0600

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + Mafuz Rahaman</h1>
+      <h1>Vite + Mafuz Vai</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
